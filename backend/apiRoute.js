26f7461a95774e6230c137e3c10f1f20647a322d
Nikita <emper137137@gmail.com>
2022-03-19 20:16:32 +1000

    basic backend functionality BUT without proper validation

--- backend/apiRoute.js
+++ backend/apiRoute.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Payment = require('./paymentSchema');
 
 const router = express.Router();
 
 router.post('/submitPayment', (req, res) => {
-    console.log('hello');
+    const newPayment = new Payment(req.body);
+    
+    newPayment
+        .save()
+        .then(payment => {
+            const { _id: RequestId, Amount } = payment;
+            res.status(200).json({ RequestId, Amount });
+        })
+        .catch(err => console.log(err)); // #
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: './backend/.env' });
 
 const app = express();
 
+app.use(express.json());
 app.use('/api', require('./apiRoute'));
+
+mongoose
+    .connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.fmalu.mongodb.net/datasub-payment-app?retryWrites=true&w=majority`)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// # useless clg's
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening to ${port}`));
