a2119fd0e58676eeb986e06bbe8cea7e6c0af2fb
Nikita <emper137137@gmail.com>
2022-03-19 23:15:31 +1000

    boilerplate for client is ready

--- backend/apiRoute.js
+++ backend/apiRoute.js
@@ -19,7 +19,7 @@ router.post('/submitPayment', (req, res) => {
         })
         .catch(err => {
             res.status(400).json({ err });
-        }); // #
+        });
 });
 
 module.exports = router;
