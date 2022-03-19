5c5c08df11ebf01fd2d70bf3665122319ce95952
Nikita <emper137137@gmail.com>
2022-03-20 01:13:51 +1000

    CardNumber client validation

--- client/src/components/CardNumber.js
+++ client/src/components/CardNumber.js
@@ -1,10 +1,34 @@
+// Styles
 import {
     FormGroup,
     Input,
-    Label
+    Label,
+    FormFeedback
 } from 'reactstrap';
+// Hooks
+import { useState, useEffect } from 'react';
+// Helpers
+import { isStringLength, isNumber } from '../helpers/validation';
 
 export const CardNumber = () => {
+    const [cardNumber, setCardNumber] = useState('');
+    const [isInvalid, setIsInvalid] = useState(false);
+    const [isValid, setIsValid] = useState(false);
+
+    const handleOnChange = ({ target }) => setCardNumber(target.value);
+
+    // validation
+    useEffect(() => {
+        if (cardNumber === '') return setIsInvalid(false);
+        if (isStringLength(cardNumber, 16) && isNumber(cardNumber)) {
+            setIsInvalid(false);
+            setIsValid(true);
+            return null;
+        }
+        setIsInvalid(true);
+        setIsValid(false);
+    }, [cardNumber]);
+
     return (
         <FormGroup floating>
             <Input 
@@ -13,10 +37,16 @@ export const CardNumber = () => {
                 placeholder="Card Number"
                 type="text"
                 autoComplete="cc-number"
+                onChange={handleOnChange}
+                invalid={isInvalid}
+                valid={isValid}
             />
             <Label for="cardNumber">
                 Card Number
             </Label>
+            <FormFeedback tooltip>
+                Invalid
+            </FormFeedback>
         </FormGroup>
     )
 }
