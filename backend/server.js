3cca1667a841f742362047a329ce409d822cc4b1
Nikita <emper137137@gmail.com>
2022-03-20 20:31:09 +1000

    deploy version 1

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,23 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: './backend/.env' });
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api', require('./apiRoute'));
 
 mongoose
-    .connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.fmalu.mongodb.net/datasub-payment-app?retryWrites=true&w=majority`)
+    .connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.fmalu.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
+// Serve static assets in PRODUCTION
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+    
 app.listen(port, () => console.log(`Listening to ${port}`));
