be9def222e3ab0568ec6e8d7aa62938791c00041
Nikita <emper137137@gmail.com>
2022-03-20 10:37:12 +1000

    SubmitButton state and redux state setup

--- client/src/components/SubmitButton.js
+++ client/src/components/SubmitButton.js
@@ -4,12 +4,45 @@ import {
     Button
 } from 'reactstrap';
 import styles from './SubmitButton.module.css';
+// React Hooks
+import { useState, useEffect } from 'react';
+// Redux Hooks & action dispatchers
+import { useSelector } from 'react-redux';
 
 export const SubmitButton = () => {
+    // button props
+    const buttonProps = {
+        valid: {
+            classNames: styles.submitBtn,
+            color: 'dark',
+            active: true
+        },
+        invalid: {
+            classNames: styles.submitBtn + ' ' + styles.invalid,
+            color: 'light',
+            active: false
+        }
+    }
+
+    // Internal state setup
+    const [isValid, setIsValid] = useState('invalid');
+    
+    // Redux state setup
+    const apiSlice = useSelector(({ apiSlice }) => apiSlice);
+    useEffect(() => {
+        const areAllValid = Object.values(apiSlice).every(value => Boolean(value));
+        areAllValid ? setIsValid('valid') : setIsValid('invalid');
+    }, [apiSlice]);
+    
     return (
         <Col xs="12" sm="6" className="mb-3">
-            <Button className={styles.submitBtn}>
-                Pay
+            <Button
+                className={buttonProps[isValid].classNames}
+                color={buttonProps[isValid].color}
+                active={buttonProps[isValid].active}
+                disabled={!buttonProps[isValid].active}
+            >
+                Submit payment
             </Button>
         </Col>
     )
